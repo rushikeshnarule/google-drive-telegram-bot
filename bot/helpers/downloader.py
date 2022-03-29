b65a1ff6fef80ce9e72fb1885062cdea7d549f71
rushikeshnarule <75885428+rushikeshnarule@users.noreply.github.com>
2022-03-30 02:17:44 +0530

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -10,7 +10,7 @@ from bot import DOWNLOAD_DIRECTORY, LOGGER
 
 def download_file(url, dl_path):
   try:
-    dl = SmartDL(url, dl_path, progress_bar=False)
+    dl = SmartDL(url, dl_path, progress_bar=True)
     LOGGER.info(f'Downloading: {url} in {dl_path}')
     dl.start()
     return True, dl.get_dest()
